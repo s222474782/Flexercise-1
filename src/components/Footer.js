c7f8d7235abdc6ab7c7570413d23eeede0a04514
s222474782 <118607327+s222474782@users.noreply.github.com>
2023-01-30 11:38:32 +0300

    Changes on the footer logo
    the logo had wrong sizing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return(
 <Box mt="80px" bgcolor="#CCCCCC">
 <Stack gap="40px" alignItems="center" px="40px" pt="24px">
-<img src={Logo} alt="logo" width="200px" height="40px" />
+<img src={Logo} alt="logo" width="150px" height="90px" />
 <Typography variant="h5" pb="40px" mt="20px">
   Made by Marek Przybylkiewicz and Team
 </Typography>
